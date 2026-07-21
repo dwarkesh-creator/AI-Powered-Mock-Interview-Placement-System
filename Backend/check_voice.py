c00332a512e6a524619626ed212cfb66e7c06c58
dwarkesh-creator <65116950+dwarkesh-creator@users.noreply.github.com>
2026-07-21 14:56:41 +0530

    clean up unnecessary files

--- Backend/check_voice.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-from tts_lipsync import DEFAULT_TTS_VOICE
-
-print(f'Default voice in code: {DEFAULT_TTS_VOICE}')
-print(f'Voice from .env: {os.getenv("GEMINI_TTS_VOICE", "Not set")}')
-print(f'\n✅ Voice will be: {os.getenv("GEMINI_TTS_VOICE") or DEFAULT_TTS_VOICE}')
